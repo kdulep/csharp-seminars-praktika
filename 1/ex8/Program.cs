778a0e0fde29489b50573020666ff2f6ba0ded7b
Konstantin Doulepov <kdulep@gmail.com>
2022-08-07 00:33:02 +0500

    praktika1 ex4 ex6 ex8 minor changes

--- 1/ex8/Program.cs
+++ 1/ex8/Program.cs
@@ -1,6 +1,8 @@
-﻿int number= Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Input number");
+int number= Convert.ToInt32(Console.ReadLine());
 int count = 2;
 
+Console.WriteLine("Even numbers");
 while (count<=number){
 Console.WriteLine(count);
 count+=2;
