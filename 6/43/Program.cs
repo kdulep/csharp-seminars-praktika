f0f3920c62e56b76baee7ad69d31d2287ad6d638
Konstantin Doulepov <kdulep@gmail.com>
2022-08-27 04:29:26 +0500

    seminar 6 small additions

--- 6/43/Program.cs
+++ 6/43/Program.cs
@@ -2,7 +2,7 @@
 
 Console.OutputEncoding = Encoding.UTF8;
 
-int InputNumber(in string prompt)
+double InputNumber(in string prompt)
 {
     bool result;
     do
@@ -10,14 +10,14 @@ int InputNumber(in string prompt)
         Console.WriteLine(prompt);
         string? input = Console.ReadLine();
 
-        result = int.TryParse(input, out var number);
+        result = double.TryParse(input, out var number);
         if (result == true)
         {
             //Console.WriteLine($"Преобразование прошло успешно. Число: {number}");
             return number;
         }
         else
-            Console.WriteLine("Преобразование завершилось неудачно");
+            Console.WriteLine("Преобразование завершилось неудачно. Повторите ввод");
     } while (result);
     return 0;
 }
@@ -27,8 +27,8 @@ double k1 = InputNumber("Введите k1:");
 double b2 = InputNumber("Введите b2:");
 double k2 = InputNumber("Введите k2:");
 
-double x = (double)(b2 - b1) / (k1 - k2);
-double y = (double)(k1 * (b2 - b1)) / (k1 - k2) + b1;
+double x = (b2 - b1) / (k1 - k2);
+double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
 
-Console.WriteLine($"Точка пересечения: [{x}:{y}]");
+Console.WriteLine($"Точка пересечения: [{x};{y}]");
 
